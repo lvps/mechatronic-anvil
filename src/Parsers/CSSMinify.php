15e61b86b246b2e9809bec4d72785c2dac8b7d0d
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2016-12-17 18:51:49 +0100

    CSS "minifier" from Corkhammer

---
+++ src/Parsers/CSSMinify.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright (c) 2016 Ludovico Pavesi
+ * Released under The MIT License (see LICENSE)
+ */
+
+namespace lvps\MechatronicAnvil\Parsers;
+use lvps\MechatronicAnvil\File;
+use lvps\MechatronicAnvil\Parser;
+
+class CSSMinify implements Parser {
+	private static $remove = ["\r\n", "\r", "\n", "\t", '      ', '     ', '    ', '   ', '  '];
+
+	public function canParse(File $what): bool {
+		if(strtolower($what->getExtension()) === 'css') {
+			return true;
+		}
+		return false;
+	}
+
+	public function parse(File $file) {
+		return;
+	}
+
+	public function renderToString(File $file): string {
+		return str_replace(self::$remove, '', preg_replace('#\s*/\*.+?\*/#sm', '', $file->getRenderFrom()->getContents()));
+	}
+
+	public function renderToFile(File $file) {
+		file_put_contents($file->getFilename(), $this->renderToString($file));
+		$file->applyMtime();
+		$file->applyMode();
+	}
+}
